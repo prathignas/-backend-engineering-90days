62fe3abf50f856894340de0c7a1d90ee959a3a79
prathigna <prathigna@prathignas-MacBook-Air.local>
2026-08-23 08:44:41 +0530

    add word count program

---
+++ wc.go
@@ -0,0 +1,105 @@
+// package main
+
+// import(
+// 	"fmt"
+// 	"strings"
+// )
+
+// func main (){
+// 	line:= " hello go hello"
+
+// 	words:= strings.Fields(line)
+
+// 	for _,word:=range words{
+// 		fmt.Println(word)
+// 	}
+// }
+
+
+
+// COUNTS SPECIFIC WORD USING FOR LOOP
+
+
+
+// // COUNT ONE WORD
+// package main
+
+// import(
+// 	"fmt"
+// 	"strings"
+// )
+
+// func main(){
+// 	line:="hello go hello"
+
+// 	words := strings.Fields(line)
+
+// 	counts:=0
+
+// 	for _,word :=range words{
+// 		if word=="hello"{
+// 		counts++
+// 		}
+// 	}
+// 	fmt.Println("word apprears",counts)
+// }
+
+
+//.    ADDING MAP AND COUNT EACH WORD FREQUENCY
+
+// package main
+
+// import(
+// 	"fmt"
+// 	"strings"
+
+// )
+
+// func main(){
+// 	line:= "hello go hello"
+
+// 	words := strings.Fields(line)
+
+// 	counts := make(map[string]int)
+
+// 	for _,word := range words{
+// 		counts[word]++
+// 	}
+
+// 	for word,count := range counts{
+// 		fmt.Printf("%s %d\n",word,count)
+// 	}
+// }
+
+  
+// WORD COUNT PROGRAM
+package main
+
+import(
+	"fmt"
+	"strings"
+	"os"
+)
+
+func wordcount(s string) map[string]int{
+    words := strings.Fields(s)
+
+	counts := make(map[string]int)
+
+	for _,word:=range words{
+		counts[word]++
+	}
+	return counts
+}
+
+func main(){
+	data ,err := os.ReadFile("sample.txt")
+
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+
+	result:=wordcount(string(data))
+	fmt.Println(result)
+}
